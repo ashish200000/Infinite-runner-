e5677eccba32bcfe9e3c36547c046cb64d2ee50f
ashish200000 <78677208+ashish200000@users.noreply.github.com>
2021-08-08 14:23:50 +0530

    Add files via upload

---
+++ Infinite Runner Game/sketch.js
@@ -0,0 +1,26 @@
+var witch,witchImage;
+
+
+
+function preload(){
+
+
+  
+
+}
+
+function setup() {
+  createCanvas(400, 400);
+   
+  
+  witch = createSprite(100,50,10,10);
+  
+
+
+}
+
+function draw() {
+  background(220);
+
+  drawsprite();
+}
